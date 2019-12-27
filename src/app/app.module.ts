840c0a6eb1cc546febac3b0b42a19660abd91610
Nina Dominique Thomé Bernardo - NBE <nina.bernardo@iteris.local>
2019-12-27 18:10:50 -0300

    criando a tela login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CustomMaterialModule } from './core/material.module';
+import { AppRoutingModule } from './core/app-routing.module';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { LogarComponent } from './logar/logar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    LogarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
